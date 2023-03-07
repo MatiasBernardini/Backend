b3a577f15988e8e648d9046c183981fffe69bcfb
matiasbernardini <bernardinimatias14@gmail.com>
2023-03-07 14:45:45 -0300

    descargando handlebars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express"
+import { engine } from 'express-handlebars';
+import __dirname from "./utils.js"
 import productRouter from "./routes/products.router.js";
 import cartManager from "./cart.js"
 import cartsRouter from "./routes/cart.routes.js";
 
 const app = express()
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
 const cartManagerr = new cartManager ("./Cart.json")
 
@@ -11,6 +17,10 @@ app.use ("/api/products", productRouter)
 app.use("/api/carts", cartsRouter)
 
 app.use (express.json())
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.listen (8080, () => {
     console.log ( "Servidor escuchado en el puerto 8080" )
